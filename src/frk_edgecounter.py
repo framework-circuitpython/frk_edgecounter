293f32a56a3d1d2663f7bc5c94d33391093b4221
Nathan Woody <nathan@nathanwoody.com>
2022-12-28 05:38:52 +0000

    Commit to latest

--- src/frk_edgecounter.py
+++ src/frk_edgecounter.py
@@ -1,36 +1,37 @@
-from framework import Driver
+import asyncio
 import countio
 import digitalio
 
-class EdgeCounter(Driver):
-    _defaults = {'sleep': 0.0,
-                 'pull': 'UP',
-                 'edge': 'FALL',
-                 'count': 0,
-                 'reset': False,
-                 'event': False,
-                 'on_event': []}
-
-    _edges = {'RISE': countio.edge.RISE,
-              'FALL': countio.edge.FALL,
-              'RISE_AND_FALL': countio.edge.RISE_AND_FALL}
-
-    _pulls = {'NONE': None,
-              'UP': digitalio.Pull.UP,
-              'DOWN': digitalio.Pull.DOWN}
-
+class EdgeCounter:
+    sleep = 0.01
+    count = 0
+    edge = "FALL"
+    pull = "NONE"
+    reset = False
+    alarm_at = 10
+    
+    alarm = False
+    on_alarm = []
+    
+    _edges = {"RISE": countio.Edge.RISE,
+              "FALL": countio.Edge.FALL,
+              "RISE_AND_FALL": countio.Edge.RISE_AND_FALL}
+    _pulls = {"UP": digitalio.Pull.UP,
+              "DOWN": digitalio.Pull.DOWN,
+              "NONE": None}
+    
     def _init_device(self):
-        self._device = countio.Counter(self._pin, edge=self._edge, pull=self._pull)
-        self.__count = self._count = self._device.count
-
-    def _loop(self):
+        self._device = countio.Counter(self._pin, edge=self._edges[self._edge], pull=self._pulls[self._pull])
+    
+    async def _run(self):
         self._count = self._device.count
-        if self._count > self.__count:
-            self._handle_event('event', self._count)
-            self.__count = self._count
-
+        while True:
+            self._count = self._device.count
+            if self._count >= self._alarm_at:
+                self._handle_event("alarm")
+                self._device.reset()
+                self._count = self._device.count
+            await asyncio.sleep(self._sleep)
+    
     def _set_reset(self, v):
-        if v:
-            self._device.reset()
-            self.__count = self._count = self._device.count
-            self._reset = False
+        if v: self._device.reset()
